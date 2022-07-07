8eec5f491d7e7396617ba18cd47bb651cb5c7995
Gurudev Singh <gurudev620.gs@gmail.com>
2022-07-07 22:11:43 +0530

    navigation and logger added in auth, login and register screens

--- lib/view/Screens/LoginScreen/login_screen.dart
+++ lib/view/Screens/LoginScreen/login_screen.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:logging/logging.dart';
 
 class LoginScreen extends StatelessWidget {
-  const LoginScreen({Key? key}) : super(key: key);
+  final Logger logger=Logger('Login Screen');
+  LoginScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    logger.fine('Login Screen builded successfully');
     return Scaffold(
       body: Center(
         child: Text('login screen'),
--- lib/view/Screens/RegisterScreen/register_screen.dart
+++ lib/view/Screens/RegisterScreen/register_screen.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:logging/logging.dart';
 
 class RegisterScreen extends StatelessWidget {
-  const RegisterScreen({Key? key}) : super(key: key);
+  final Logger logger=Logger('Register Screen');
 
   @override
   Widget build(BuildContext context) {
+    logger.fine('Register screen builded successfully');
     return Scaffold(
       appBar: AppBar(),
       body: Center(
