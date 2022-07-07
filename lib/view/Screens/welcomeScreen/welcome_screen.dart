d53cb00022fbdf4b97371b8716dd6a552a9d5c18
Gurudev Singh <gurudev620.gs@gmail.com>
2022-07-07 14:42:35 +0530

    added image title buttons in welcome screen

--- lib/view/Screens/welcomeScreen/welcome_screen.dart
+++ lib/view/Screens/welcomeScreen/welcome_screen.dart
@@ -1,6 +1,6 @@
-// ignore_for_file: unused_import
-
 import 'package:flutter/material.dart';
+import 'package:shopybee/Ui_blocks/TransparentRoundRectButton.dart';
+import 'package:shopybee/Ui_blocks/coloredRoundRectButton.dart';
 
 class WelcomeScreen extends StatelessWidget {
   const WelcomeScreen({Key? key}) : super(key: key);
@@ -10,7 +10,35 @@ class WelcomeScreen extends StatelessWidget {
     return SafeArea(
       child: Scaffold(
         body: Column(
-          children: [Image.asset('assets/images/welcome_banner.jpg')],
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.asset('assets/images/welcome_banner.png'),
+            Container(
+              padding: EdgeInsets.all(10),
+              // ignore: prefer_const_literals_to_create_immutables
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    Text('Welcome to',
+                        style: TextStyle(
+                            fontSize: 40,
+                            fontFamily: 'mukta',
+                            fontWeight: FontWeight.w500)),
+                    Text(
+                      'Shopybee',
+                      style: TextStyle(
+                          fontSize: 60,
+                          fontWeight: FontWeight.bold,
+                          fontStyle: FontStyle.italic,
+                          color: Colors.deepOrangeAccent),
+                    ),
+                    ColoredRoundRectButton(
+                      text: 'Signin',
+                    ),
+                    TransparentRoundRectButton(text: 'Login')
+                  ]),
+            )
+          ],
         ),
       ),
     );
