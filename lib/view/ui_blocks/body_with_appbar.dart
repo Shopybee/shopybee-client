8783f37aa2a59d576f8288213e5031284fde479b
Alpha17-2 <alphaisgod1@gmail.com>
2022-07-10 11:08:33 +0530

    display location of user in the app bar

--- lib/view/ui_blocks/body_with_appbar.dart
+++ lib/view/ui_blocks/body_with_appbar.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:shopybee/services/navbar_icons/appbar_icons.dart';
 import 'package:shopybee/uitls/device_size.dart';
 import 'package:shopybee/view/Screens/AppScreen/components/search_box.dart';
 
 class BodyWithAppBar extends StatelessWidget {
-  
   final dynamic child;
   BodyWithAppBar({required this.child});
 
@@ -15,6 +15,45 @@ class BodyWithAppBar extends StatelessWidget {
           SliverOverlapAbsorber(
             handle: NestedScrollView.sliverOverlapAbsorberHandleFor(context),
             sliver: SliverAppBar(
+              title: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Icon(
+                    CustomAppbar.location,
+                    color: Colors.red[600]!,
+                  ),
+                  const SizedBox(
+                    width: 5,
+                  ),
+                  Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(
+                          width: displayWidth(context) * 0.35,
+                          child: const Text('Gamharia',
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontFamily: 'mutka',
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 17)),
+                        ),
+                        SizedBox(
+                          width: displayWidth(context) * 0.35,
+                          child: const Text(
+                            'Chhota Gamharia',
+                            overflow: TextOverflow.ellipsis,
+                            maxLines: 1,
+                            style: TextStyle(
+                                color: Colors.black54,
+                                fontSize: 13.5,
+                                fontWeight: FontWeight.w500),
+                          ),
+                        )
+                      ]),
+                ],
+              ),
               iconTheme: const IconThemeData(color: Colors.black87),
               toolbarHeight: displayHeight(context) * 0.08,
               backgroundColor: Colors.white,
