81d1ea0015dc5b240e6ebcc8e3e43fcf5cc61f11
Alpha17-2 <alphaisgod1@gmail.com>
2022-08-08 01:12:03 +0530

    modified a bit

--- lib/services/api/put_service.dart
+++ lib/services/api/put_service.dart
@@ -3,24 +3,24 @@ import 'package:http/http.dart' as http;
 import 'package:logging/logging.dart';
 import 'package:shopybee/constants/constants.dart';
 
-
 class PutService {
   Logger logger = Logger('PutService');
   Future<dynamic> put(
       {required String endUrl,
-        required Map<String, dynamic> data,
-        String? message,
-        required bool showMessage}) async {
+      required String jsondata,
+      String? message,
+      required bool showMessage}) async {
+    logger.info("Put Body : $jsondata");
     try {
-      final response =
-      await http.put(Uri.parse(baseUrl + endUrl), body: json.encode(data));
+      final response = await http.put(Uri.parse(baseUrl + endUrl),
+          body: jsondata, headers: {'Content-Type': "application/json"});
+      logger.info('Put response :${response.body}');
       switch (response.statusCode) {
         case 200:
           return response;
 
         case 400:
           break;
-
       }
     } catch (error) {
       logger.shout(error);
