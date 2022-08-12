8afe1a692fcd0831a7f25924cd6dc94e85f189c6
Alpha17-2 <alphaisgod1@gmail.com>
2022-08-13 00:46:33 +0530

    order sceen

--- lib/view/Screens/OrderScreen/order_screen.dart
+++ lib/view/Screens/OrderScreen/order_screen.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:shopybee/constants/constants.dart';
+import 'package:shopybee/controllers/order_screen_controller.dart';
+import 'package:shopybee/uitls/enums.dart';
+import 'package:shopybee/view/Screens/OrderScreen/components/bottom_sheet_order_screen.dart';
+import 'package:shopybee/view/Screens/OrderScreen/components/order_screen_status_topbar.dart';
+import 'package:shopybee/view/Screens/OrderScreen/components/order_summary_body.dart';
+import 'package:shopybee/view/Screens/OrderScreen/components/select_address_body.dart';
 
 class OrderScreen extends StatelessWidget {
   const OrderScreen({Key? key}) : super(key: key);
@@ -8,8 +15,40 @@ class OrderScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: primaryColor,
-        title: Text("Order Summary"),
+          elevation: 0,
+          backgroundColor: primaryColor,
+          title: Consumer<OrderScreenController>(
+            builder: (context, controller, child) {
+              switch (controller.navigationStatus) {
+                case OrderScreenNavigationStatus.address:
+                  return const Text("Select Delivery Address");
+                case OrderScreenNavigationStatus.summary:
+                  return const Text("Order Summary");
+
+                case OrderScreenNavigationStatus.payment:
+                  return const Text("Payment");
+              }
+            },
+          )),
+      bottomSheet: const BottomSheetOrderScreen(),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          const OrderScreenStatusTopbar(),
+          const SizedBox(height: 10),
+          Expanded(child: Consumer<OrderScreenController>(
+            builder: (context, controller, child) {
+              switch (controller.navigationStatus) {
+                case OrderScreenNavigationStatus.address:
+                  return SelectAddressBody();
+                case OrderScreenNavigationStatus.summary:
+                  return OrderSummaryBody();
+                case OrderScreenNavigationStatus.payment:
+                  return SelectAddressBody();
+              }
+            },
+          )),
+        ],
       ),
     );
   }
